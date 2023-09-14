fefe308416e648796cf676a49ef46e77232b235a
Laurent Glorian <laurentglorian812@gmail.com>
2023-09-14 11:36:13 +0700

    Add files via upload

---
+++ TreasureHunter/Isi.java
@@ -0,0 +1,44 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import javax.swing.JOptionPane;
+/**
+ * Write a description of class Isi here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Isi extends Actor
+{
+    /**
+     * Act - do whatever the Isi wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public int nilai=-1;
+    public void act() 
+    {
+        // Add your action code here.
+        if(Greenfoot.mouseClicked(this)){
+            int xh=((Papan)getWorld()).x;
+            int yh=((Papan)getWorld()).y;
+            int xo=((Papan)getWorld()).xo;
+            int yo=((Papan)getWorld()).yo;
+            int j=((Papan)getWorld()).hitungjarak(xo,yo,xh,yh);
+            int x=Greenfoot.getMouseInfo().getX()/16;
+            int y=Greenfoot.getMouseInfo().getY()/16;
+            int j2=((Papan)getWorld()).hitungjarak(x,y,xh,yh);
+            if(nilai==0){ setImage("empty0.png");
+                if(j2<j)
+                JOptionPane.showMessageDialog(null, "Makin dekat");
+                else
+                JOptionPane.showMessageDialog(null, "Makin jauh");
+                ((Papan)getWorld()).xo=x;
+                ((Papan)getWorld()).yo=y;
+    } 
+    else{
+        setImage("empty1.png");
+        JOptionPane.showMessageDialog(null, "dapat");
+}
+}
+}
+public void setnilai(int n){nilai=n;}
+public int getnilai(){return(nilai);}
+}
---
+++ TreasureHunter/Papan.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Papan here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Papan extends World
+{
+
+    /**
+     * Constructor for objects of class Papan.
+     * 
+     */
+    public static final int maxx=20;
+    public static final int maxy=20;
+    public int x=Greenfoot.getRandomNumber(maxx);
+    public int y=Greenfoot.getRandomNumber(maxy);
+    public int xo=1000;
+    public int yo=1000;
+    Isi dt[][]=new Isi[maxx][maxy];
+    public Papan()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(16*maxx, 16*maxy, 1);
+        int px=0,py=0;
+        for(int i=0;i<maxy;i++){
+            for(int j=0;j<maxx;j++){
+                dt[j][i]=new Isi();
+                dt[j][i].setnilai(0);
+                dt[j][i].setImage("default.png");
+                addObject(dt[j][i],j*16+8,i*16+8);
+    }
+}
+dt[x][y].setnilai(1);
+}public int hitungjarak(int x1,int y1,int x2,int y2){
+    return(Math.abs(x2-x1)+Math.abs(y2-y1));
+}
+}
